ffa746e9438ce252f86cb226022a46c34bb02691
dev-jssin <dev.jssin@gmail.com>
2023-12-31 01:20:34 +0900

    feat: Add Base Setting

---
+++ backend/src/main/java/com/backend/core/common/dto/ResponseDto.java
@@ -0,0 +1,28 @@
+package com.backend.core.common.dto;
+
+import com.backend.core.common.enums.StatusCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class ResponseDto {
+
+    private final Boolean success;
+    private final Integer code;
+    private final String message;
+
+    public static ResponseDto of(Boolean success, StatusCode statusCode) {
+        return new ResponseDto(success, statusCode.getCode(), statusCode.getMessage());
+    }
+
+    public static ResponseDto of(Boolean success, StatusCode statusCode, Exception e) {
+        return new ResponseDto(success, statusCode.getCode(), statusCode.getMessage(e));
+    }
+
+    public static ResponseDto of(Boolean success, StatusCode statusCode, String message) {
+        return new ResponseDto(success, statusCode.getCode(), statusCode.getMessage(message));
+    }
+}
