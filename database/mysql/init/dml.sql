fd95baef4bf6f8d5795e5835bbca589251c65e4b
dev-jssin <dev.jssin@gmail.com>
2024-01-07 01:09:57 +0900

    chore: Docker Compose Update

--- database/mysql/init/dml.sql
+++ database/mysql/init/dml.sql
@@ -1,2 +1,1 @@
-/* ID : user , PWD : password */
-INSERT INTO TN_USER (USER_ID, PASSWORD) VALUES ('user', '$2a$12$GymeXQYR./CClgh9MVrL0./6W7XmZctXyPzrLSf37smmnKcdFZEMS');
+INSERT INTO TN_USER (USER_ID, PASSWORD, AUTH_TYPE) VALUES ('user', '$2a$12$GymeXQYR./CClgh9MVrL0./6W7XmZctXyPzrLSf37smmnKcdFZEMS', 'US');  -- ID : user , PWD : password
